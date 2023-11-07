0828030fd07a60aafff325b342f23a027cfabf7a
Rafael Fermino <rafael_fermino_10@hotmail.com>
2023-11-07 08:38:03 -0300

    continur course, change index.html and main.js -- second lesson

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-document.querySelector('.tecla_pom');
+/* Função realizar o som */
+function tocoSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+/* clique botão Pom */
+document.querySelector('.tecla_pom').onclick = tocoSomPom;
