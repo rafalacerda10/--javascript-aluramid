879c0453d41ccc8da5dbab8a5ab63c6ca99b7122
Rafael Fermino <rafael_fermino_10@hotmail.com>
2023-11-08 10:47:07 -0300

    Reduze code and list_board

--- main.js
+++ main.js
@@ -10,3 +10,4 @@ function tocoSomPom() {
 // Aqui estamos pegando a referencia lista de teclas e atribuindo a ela a lista de teclas pelo query
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+listaDeTeclas[0].onclick = tocoSomPom;
