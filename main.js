915ed3e93b2c0ed5029304520c181ee904fa2d15
Rafael Fermino <rafael_fermino_10@hotmail.com>
2023-11-10 08:44:58 -0300

    modified

--- main.js
+++ main.js
@@ -1,24 +1,50 @@
-/* Função realizar o som */
-function tocoSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+// /* Função realizar o som */
+// function tocaSom (idElementAudio) {
+
+//     document.querySelector(idElementAudio).play();
+// }
+// /* clique botão Pom */
+// // document.querySelector('.tecla_pom').onclick = tocoSomPom;
+
+// // document.querySelectorAll('.tecla')
+
+// // Aqui estamos pegando a referencia lista de teclas e atribuindo a ela a lista de teclas pelo query
+// const listaDeTeclas = document.querySelectorAll('.tecla');
+
+// listaDeTeclas[0].onclick = tocoSomPom;
+
+
+// let contador =0;
+
+// while (contador < 9 ) {
+//     listaDeTeclas[contador].onclick = tocoSom;
+
+//     contador = contador + 1;
+
+//     console.log(contador);
+// }
+
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-/* clique botão Pom */
-// document.querySelector('.tecla_pom').onclick = tocoSomPom;
 
-// document.querySelectorAll('.tecla')
-
-// Aqui estamos pegando a referencia lista de teclas e atribuindo a ela a lista de teclas pelo query
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocoSomPom;
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+    
+    // constantes
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    // template string
+    const idAudio = `#som_${instrumento}`;
 
 
-let contador =0;
-
-while (contador < 9 ) {
-    listaDeTeclas[contador].onclick = tocoSomPom;
-
+    tecla.onclick =  function () {
+        tocaSom('idAudio')
+    }
     contador = contador + 1;
-
     console.log(contador);
 }
