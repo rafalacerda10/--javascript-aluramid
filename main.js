58f650bd71c73a9f0a3e502e3e901c104331312f
Rafael Fermino <rafael_fermino_10@hotmail.com>
2023-11-14 07:46:24 -0300

    repage

--- main.js
+++ main.js
@@ -1,50 +1,16 @@
-// /* Função realizar o som */
-// function tocaSom (idElementAudio) {
-
-//     document.querySelector(idElementAudio).play();
-// }
-// /* clique botão Pom */
-// // document.querySelector('.tecla_pom').onclick = tocoSomPom;
-
-// // document.querySelectorAll('.tecla')
-
-// // Aqui estamos pegando a referencia lista de teclas e atribuindo a ela a lista de teclas pelo query
-// const listaDeTeclas = document.querySelectorAll('.tecla');
-
-// listaDeTeclas[0].onclick = tocoSomPom;
-
-
-// let contador =0;
-
-// while (contador < 9 ) {
-//     listaDeTeclas[contador].onclick = tocoSom;
-
-//     contador = contador + 1;
-
-//     console.log(contador);
-// }
-
-function tocaSom (idElementoAudio) {
+function tocaSom (idElementoAudio) { 
     document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
+const listaTeclas = document.querySelector('.tecla');
 let contador = 0;
 
-//enquanto
-while (contador < listaDeTeclas.length) {
-    
-    // constantes
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    // template string
-    const idAudio = `#som_${instrumento}`;
-
-
-    tecla.onclick =  function () {
-        tocaSom('idAudio')
+//while , enquanto
+while (contador < listaTeclas.length) {
+    listaTeclas[contador].onclick = function () {
+        tocaSom('#som_tecla_pom');
     }
     contador = contador + 1;
+
     console.log(contador);
 }
